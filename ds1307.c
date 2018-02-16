55d3f00c6dfd15ea615b9c2ee51a6c0e1427dc5a
ziggi <borodin@unix7.org>
2018-02-16 12:33:46 +0200

    add some DS_ macros

--- ds1307.c
+++ ds1307.c
@@ -1,14 +1,19 @@
 /* $Id$ */
 
 #define DS1307_ADDR      0x68
-#define I2CDEV_READY    0
-#define I2CDEV_FAIL     1
+#define I2C_READY    0
+#define I2C_FAIL     1
 
 #include <twim.h>
 #include <ds1307.h>
 
+#define DS_SEC  0
+#define DS_MIN  1
+#define DS_HOUR 2
+
+
 uint8_t ds_write(uint8_t reg, uint8_t data) {
-    if(i2c_start((DS1307_ADDR << 1) | I2C_WRITE) == I2CDEV_FAIL) 
+    if(i2c_start((DS1307_ADDR << 1) | I2C_WRITE) == I2C_FAIL) 
         return(0);
 
     i2c_write(reg);
@@ -19,7 +24,7 @@ uint8_t ds_write(uint8_t reg, uint8_t data) {
 
 uint8_t ds_read(uint8_t reg) {
     volatile uint8_t data;
-    if(i2c_start((DS1307_ADDR << 1) | I2C_WRITE) == I2CDEV_FAIL) 
+    if(i2c_start((DS1307_ADDR << 1) | I2C_WRITE) == I2C_FAIL) 
         return(0);
     i2c_write(reg);
     i2c_rep_start((DS1307_ADDR << 1) | I2C_READ);
@@ -37,43 +42,47 @@ uint8_t ds_dec2bcd(uint8_t val) {
 }
 
 uint8_t ds_get_sec(void) {
-    uint8_t r = ds_read(0x00);
+    uint8_t r = ds_read(DS_SEC);
     r &= 0x7F;
     return(ds_bcd2dec(r));
 }
 
 uint8_t ds_get_min(void) {
-    uint8_t r = ds_read(0x01);
+    uint8_t r = ds_read(DS_MIN);
     return(ds_bcd2dec(r));
 }
 
 uint8_t ds_get_hour(void) {
-    uint8_t r = ds_read(0x02);
+    uint8_t r = ds_read(DS_HOUR);
     return(ds_bcd2dec(r));
 }
 
 
 void ds_set_sec(uint8_t num) {
-    uint8_t r = ds_write(0x00, ds_dec2bcd(num));
+    uint8_t r = ds_write(DS_SEC, ds_dec2bcd(num));
 }
 
 void ds_set_min(uint8_t num) {
-    uint8_t r = ds_write(0x01, ds_dec2bcd(num));
+    uint8_t r = ds_write(DS_MIN, ds_dec2bcd(num));
 }
 
 void ds_set_hour(uint8_t num) {
-    uint8_t r = ds_write(0x02, ds_dec2bcd(num));
+    uint8_t r = ds_write(DS_HOUR, ds_dec2bcd(num));
 }
+
+#define DS_OSC  7
+
 
 void ds_init(void) {
     /* Start oscillator */
-    uint8_t r = ds_read(0x00);
-    r &= 0x7F;
-    ds_write(0x00, r);
+    uint8_t r = ds_read(DS_SEC);
+    r &= ~(1 << DS_OSC); //0x7F;
+    ds_write(DS_SEC, r);
+
     /* Set 24h format */
-    r = ds_read(0x02);
+    r = ds_read(DS_HOUR);
     r &= 0x5F;
-    ds_write(0x00, r);
+    ds_write(DS_HOUR, r);
 }
 
 
