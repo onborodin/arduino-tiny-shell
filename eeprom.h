8c2239fc48abe1c5bb50713917c011fa3436d147
ziggi <borodin@unix7.org>
2018-03-01 00:30:09 +0200

    add spi.h eeprom.h

---
+++ eeprom.h
@@ -0,0 +1,7 @@
+
+/* $Id$ */
+
+void eeprom_write(uint16_t address, uint8_t data);
+uint8_t eeprom_read(uint16_t address);
+
+/* EOF */
---
+++ spi.h
@@ -0,0 +1,5 @@
+/* $Id$ */
+void spi_init(void);
+void spi_write(uint8_t data);
+uint8_t spi_read(void);
+/* EOF */
